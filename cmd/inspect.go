1cd8d578818ef340f6324a244cdc1f274619f719
Xackery Xtal <xackery@gmail.com>
2022-01-20 21:56:29 -0800

    Added mod support to inspect

--- cmd/inspect.go
+++ cmd/inspect.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/xackery/quail/dump"
 	"github.com/xackery/quail/eqg"
+	"github.com/xackery/quail/mod"
 )
 
 // inspectCmd represents the inspect command
@@ -48,12 +50,27 @@ Supported extensions:
 			fmt.Println("Error: open:", err)
 			os.Exit(1)
 		}
-		e := &eqg.EQG{}
-		err = e.Load(f)
-		if err != nil {
-			fmt.Printf("Error: load %s: %s\n", filepath.Base(path), err)
-			os.Exit(1)
+		defer f.Close()
+		ext := filepath.Ext(path)
+		switch strings.ToLower(ext) {
+		case ".eqg":
+			e := &eqg.EQG{}
+			err = e.Load(f)
+			if err != nil {
+				fmt.Printf("Error: load %s: %s\n", filepath.Base(path), err)
+				return nil
+			}
+		case ".mod":
+			e := &mod.MOD{}
+			err = e.Load(f)
+			if err != nil {
+				fmt.Printf("Error: load %s: %s\n", filepath.Base(path), err)
+				return nil
+			}
+		default:
+			fmt.Printf("Error: inspect: unknown extension %s on path %s", ext, filepath.Base(path))
 		}
+
 		return nil
 	},
 }
