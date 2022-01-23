918034094901a7549a533decf3702055ee6019e3
Xackery Xtal <xackery@gmail.com>
2022-01-23 00:18:39 -0800

    Cleaned up README and inspect

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# quail
-everQuest Universal Archive Import and Ligma system
+# quail - Ever[Q]uest [U]niversal [A]rchive, [I]mport and [L]oader tool
+
+Extension|CLI|Library
+---|---|---
+ani|inspect|load
+blend|TBD|TBD
+eqg|compress,extract,inspect|load,save
+mod|inspect|load,save,importGLTF
+s3d|TBD|TBD
+ter|inspect|load,save
+tog|TBD|save
+zon|inspect|load,save,importGLTF
--- cmd/inspect.go
+++ cmd/inspect.go
@@ -2,11 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/xackery/quail/ani"
 	"github.com/xackery/quail/dump"
 	"github.com/xackery/quail/eqg"
 	"github.com/xackery/quail/mod"
@@ -65,40 +67,36 @@ Supported extensions: eqg, zon, ter, ani, mod
 			os.Exit(1)
 		}
 		defer f.Close()
-		ext := filepath.Ext(path)
-		switch strings.ToLower(ext) {
-		case ".eqg":
-			e := &eqg.EQG{}
-			err = e.Load(f)
-			if err != nil {
-				fmt.Printf("Error: load %s: %s\n", filepath.Base(path), err)
-				return nil
-			}
-		case ".mod":
-			e := &mod.MOD{}
-			err = e.Load(f)
-			if err != nil {
-				fmt.Printf("Error: load %s: %s\n", filepath.Base(path), err)
-				return nil
-			}
-		case ".ter":
-			e := &ter.TER{}
-			err = e.Load(f)
-			if err != nil {
-				fmt.Printf("Error: load %s: %s\n", filepath.Base(path), err)
-				return nil
-			}
-		case ".zon":
-			e := &zon.ZON{}
-			err = e.Load(f)
-			if err != nil {
-				fmt.Printf("Error: load %s: %s\n", filepath.Base(path), err)
-				return nil
-			}
-		default:
-			fmt.Printf("Error: inspect: unknown extension %s on path %s", ext, filepath.Base(path))
+		ext := strings.ToLower(filepath.Ext(path))
+		type loader interface {
+			Load(io.ReadSeeker) error
+		}
+		type loadTypes struct {
+			instance  loader
+			extension string
+		}
+		loads := []*loadTypes{
+			{instance: &ani.ANI{}, extension: ".ani"},
+			{instance: &eqg.EQG{}, extension: ".eqg"},
+			{instance: &mod.MOD{}, extension: ".mod"},
+			{instance: &ter.TER{}, extension: ".ter"},
+			{instance: &zon.ZON{}, extension: ".zon"},
 		}
 
+		for _, v := range loads {
+			if ext != v.extension {
+				continue
+			}
+			err = v.instance.Load(f)
+			if err != nil {
+				fmt.Printf("Error: load %s: %s\n", v.extension, err)
+				os.Exit(1)
+			}
+			return nil
+		}
+
+		fmt.Printf("Error: inspect: unknown extension %s on file %s\n", ext, filepath.Base(path))
+		os.Exit(1)
 		return nil
 	},
 }
