c4ac1bc23cb28f56b572190c936ca7812dfcf932
Xackery Xtal <xackery@gmail.com>
2022-07-07 23:07:06 -0700

    Added viewer improvements

--- cmd/view.go
+++ cmd/view.go
@@ -5,20 +5,23 @@ import (
 	"fmt"
 	"image/color"
 	"io"
+	"math"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"github.com/kvartborg/vector"
 	"github.com/solarlune/tetra3d"
+	"github.com/solarlune/tetra3d/colors"
 	"github.com/spf13/cobra"
 	"github.com/xackery/quail/common"
 	"github.com/xackery/quail/mod"
 	"github.com/xackery/quail/ter"
+	"golang.org/x/image/font/basicfont"
 )
-
-const screenWidth = 398
-const screenHeight = 224
 
 // viewCmd represents the view command
 var viewCmd = &cobra.Command{
@@ -98,7 +101,14 @@ Supported extensions: mod
 				return fmt.Errorf("export gltf %s: %w", v.extension, err)
 			}
 
-			v := &viewer{}
+			v := &viewer{
+				width:         796,
+				height:        448,
+				drawDebugText: true,
+			}
+
+			//data, _ := ioutil.ReadFile("example.gltf")
+			//err = v.load(data)
 			err = v.load(buf.Bytes())
 			if err != nil {
 				return fmt.Errorf("load gltf to viewer: %w", err)
@@ -120,59 +130,158 @@ func init() {
 }
 
 type viewer struct {
-	scene  *tetra3d.Scene
-	camera *tetra3d.Camera
+	width, height     int
+	library           *tetra3d.Library
+	scene             *tetra3d.Scene
+	camera            *tetra3d.Camera
+	cameraTilt        float64
+	cameraRotate      float64
+	prevMousePosition vector.Vector
+
+	drawDebugText      bool
+	drawDebugDepth     bool
+	drawDebugNormals   bool
+	drawDebugCenters   bool
+	drawDebugWireframe bool
 }
 
 func (v *viewer) load(gltfData []byte) error {
-	library, err := tetra3d.LoadGLTFData(gltfData, nil)
+	var err error
+	v.library, err = tetra3d.LoadGLTFData(gltfData, nil)
 	if err != nil {
 		return fmt.Errorf("loadGLTFData: %w", err)
 	}
-	v.scene = library.ExportedScene
+	v.scene = v.library.ExportedScene
 
-	v.camera = tetra3d.NewCamera(screenWidth, screenHeight)
+	v.camera = tetra3d.NewCamera(v.width, v.height)
 	v.camera.Move(0, 0, 12)
 	v.scene.Root.AddChildren(v.camera)
+	v.scene.LightingOn = false
 
 	fmt.Println(v.scene.Root.HierarchyAsString())
 	return nil
 }
 
-func (g *viewer) Update() error {
-	if ebiten.IsKeyPressed(ebiten.KeyQ) {
-		fmt.Println("q key was pressed, quitting")
+func (v *viewer) Update() error {
+
+	moveSpd := 0.05
+	if ebiten.IsKeyPressed(ebiten.KeyQ) || ebiten.IsKeyPressed(ebiten.KeyEscape) {
+		fmt.Println("q or escape key was pressed, quitting")
 		os.Exit(0)
 	}
+	// We use Camera.Rotation.Forward().Invert() because the camera looks down -Z (so its forward vector is inverted)
+	forward := v.camera.LocalRotation().Forward().Invert()
+	right := v.camera.LocalRotation().Right()
+
+	pos := v.camera.LocalPosition()
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyV) {
+		v.drawDebugWireframe = !v.drawDebugWireframe
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyT) {
+		v.drawDebugText = !v.drawDebugText
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyN) {
+		v.drawDebugNormals = !v.drawDebugNormals
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		pos = pos.Add(forward.Scale(moveSpd))
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		pos = pos.Add(right.Scale(moveSpd))
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		pos = pos.Add(forward.Scale(-moveSpd))
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		pos = pos.Add(right.Scale(-moveSpd))
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeySpace) {
+		pos[1] += moveSpd
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyControl) {
+		pos[1] -= moveSpd
+	}
+
+	v.camera.SetLocalPosition(pos)
+
+	// Rotating the camera with the mouse
+
+	// Rotate and tilt the camera according to mouse movements
+	mx, my := ebiten.CursorPosition()
+
+	mv := vector.Vector{float64(mx), float64(my)}
+
+	diff := mv.Sub(v.prevMousePosition)
+
+	v.cameraTilt -= diff[1] * 0.005
+	v.cameraRotate -= diff[0] * 0.005
+
+	v.cameraTilt = math.Max(math.Min(v.cameraTilt, math.Pi/2-0.1), -math.Pi/2+0.1)
+
+	tilt := tetra3d.NewMatrix4Rotate(1, 0, 0, v.cameraTilt)
+	rotate := tetra3d.NewMatrix4Rotate(0, 1, 0, v.cameraRotate)
+
+	// Order of this is important - tilt * rotate works, rotate * tilt does not, lol
+	v.camera.SetLocalRotation(tilt.Mult(rotate))
+
+	v.prevMousePosition = mv.Clone()
+
+	/*	armature := v.scene.Root.ChildrenRecursive().ByName("Armature", true)[0].(*tetra3d.Node)
+		armature.Rotate(0, 1, 0, 0.01)
+
+		if inpututil.IsKeyJustPressed(ebiten.KeyF) {
+			// armature.AnimationPlayer.FinishMode = tetra3d.FinishModeStop
+			armature.AnimationPlayer().Play(v.library.Animations["ArmatureAction"])
+		}
+
+		armature.AnimationPlayer().Update(1.0 / 60)
+
+		table := v.scene.Root.Get("Table").(*tetra3d.Model)
+		table.AnimationPlayer().BlendTime = 0.1
+		if inpututil.IsKeyJustPressed(ebiten.Key1) {
+			table.AnimationPlayer().Play(v.library.Animations["SmoothRoll"])
+		}
+		if inpututil.IsKeyJustPressed(ebiten.Key2) {
+			table.AnimationPlayer().Play(v.library.Animations["StepRoll"])
+		}
+
+		table.AnimationPlayer().Update(1.0 / 60)
+	*/
 	return nil
 }
 
-func (g *viewer) Draw(screen *ebiten.Image) {
+func (v *viewer) Draw(screen *ebiten.Image) {
 
-	// Call Camera.Clear() to clear its internal backing texture. This
-	// should be called once per frame before drawing your Scene.
-	g.camera.Clear()
+	screen.Fill(color.RGBA{60, 70, 80, 255})
 
-	// Now we'll render the Scene from the camera. The Camera's ColorTexture will then
-	// hold the result.
+	v.camera.Clear()
 
-	// Below, we'll pass both the Scene and the scene root because 1) the Scene influences
-	// how Models draw (fog, for example), and 2) we may not want to render
-	// all Models.
+	v.camera.RenderNodes(v.scene, v.scene.Root)
+	opt := &ebiten.DrawImageOptions{}
+	w, h := v.camera.ColorTexture().Size()
+	opt.GeoM.Scale(float64(v.width)/float64(w), float64(v.height)/float64(h))
+	screen.DrawImage(v.camera.ColorTexture(), nil)
 
-	// Camera.RenderNodes() renders all Nodes in a tree, starting with the
-	// Node specified. You can also use Camera.Render() to simply render a selection of
-	// Models.
-	g.camera.RenderNodes(g.scene, g.scene.Root)
+	if v.drawDebugWireframe {
+		v.camera.DrawDebugWireframe(screen, v.scene.Root, colors.Red())
+	}
 
-	// Before drawing the result, clear the screen first.
-	screen.Fill(color.RGBA{20, 30, 40, 255})
+	if v.drawDebugNormals {
+		v.camera.DrawDebugNormals(screen, v.scene.Root, 0.5, colors.Blue())
+	}
 
-	// Draw the resulting texture to the screen, and you're done! You can
-	// also visualize the depth texture with g.Camera.DepthTexture.
-	screen.DrawImage(g.camera.ColorTexture(), nil)
+	if v.drawDebugText {
+		v.camera.DrawDebugText(screen, 1, colors.White())
+		txt := "T to toggle this text\nWASD: Move, Mouse: Look\n1 Key: Play [SmoothRoll] Animation On Table\n2 Key: Play [StepRoll] Animation on Table\nNote the animations can blend\nF Key: Play Animation on Skinned Mesh\nNote that the nodes move as well\nF4: Toggle fullscreen\nF6: Node Debug View\nQ: Quit"
+		text.Draw(screen, txt, basicfont.Face7x13, 0, 140, color.RGBA{255, 0, 0, 255})
+	}
 }
 
-func (g *viewer) Layout(w, h int) (int, int) {
-	return screenWidth, screenHeight
+func (v *viewer) Layout(w, h int) (int, int) {
+	return v.width, v.height
 }
--- go.mod
+++ go.mod
@@ -6,18 +6,25 @@ require github.com/qmuntal/gltf v0.20.3
 
 require (
 	github.com/g3n/engine v0.2.0
+	github.com/hajimehoshi/ebiten/v2 v2.3.5
+	github.com/kvartborg/vector v0.0.0-20210122071920-91df40ba4054
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/solarlune/tetra3d v0.9.0
+	github.com/spate/glimage v0.0.0-20200505055513-fbdcc60a65e5
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/viper v1.10.0
 	github.com/xackery/colors v1.0.1
 	github.com/xackery/go-zlib v1.1.2-0.20220114180313-3fb65525737c
-	golang.org/x/image v0.0.0-20210607152325-775e3b0c77b9
+	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220320163800-277f93cfa958 // indirect
+	github.com/gofrs/flock v0.8.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jezek/xgb v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
@@ -26,7 +33,10 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
-	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
+	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
+	golang.org/x/mobile v0.0.0-20220518205345-8578da9835fd // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
