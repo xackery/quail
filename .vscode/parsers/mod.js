f02ddb72ba18138ad307dd48e305224ef7ddf90f
Xackery Xtal <xackery@gmail.com>
2023-04-10 13:32:50 -0700

    Add binary viewer

---
+++ .vscode/parsers/mod.js
@@ -0,0 +1,33 @@
+registerFileType((fileExt, filePath, fileData) => {
+	// Check for wav extension
+	if (fileExt == 'mod') {
+		const headerArray = fileData.getBytesAt(0, 4);
+		const header = String.fromCharCode(...headerArray)
+		if (header == 'EQGM')
+			return true;
+	}
+	return false;
+});
+
+registerParser(() => {
+	// Parse
+	read(4);
+	addRow('Header', getStringValue(), 'header (EQGM)');
+	read(4);
+	addRow('Version', getNumberValue(), 'version (1, 2, 3)');
+	read(4);
+	const nameLength = getNumberValue();
+	addRow('Name', nameLength, 'name length');
+	read(4);
+	const materialCount = getNumberValue();
+	addRow('Material Count', materialCount, 'material count');
+	read(4);
+	const verticesCount = getNumberValue();
+	addRow('Vertices Count', verticesCount, 'vertices count');
+	read(4);
+	const trianglesCount = getNumberValue();
+	addRow('Triangles Count', trianglesCount, 'triangles count');
+	read(4);
+	const bonesCount = getNumberValue();
+	addRow('Bones Count', bonesCount, 'bones count');
+});
