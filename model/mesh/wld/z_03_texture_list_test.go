57de00a68be9710f16dbc75bd551caf8ac13264d
Joel <51065819+jcr4990@users.noreply.github.com>
2023-04-19 22:09:23 -0400

    Removed todo from texture_list

--- model/mesh/wld/z_03_texture_list_test.go
+++ model/mesh/wld/z_03_texture_list_test.go
@@ -33,7 +33,7 @@ func TestWLD_textureListReadWrite(t *testing.T) {
 		fragOffset int
 		wantErr    bool
 	}{
-		{name: "gequip.s3d", fragOffset: 0, wantErr: false}, // TODO: texture count is 0 on all examples
+		{name: "gequip.s3d", fragOffset: 0, wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
