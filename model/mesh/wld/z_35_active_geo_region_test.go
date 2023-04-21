2ead1b003e7ec93edcdc7c2df964360ca1394a53
Joel <51065819+jcr4990@users.noreply.github.com>
2023-04-21 13:35:33 -0400

    Update TODO

--- model/mesh/wld/z_35_active_geo_region_test.go
+++ model/mesh/wld/z_35_active_geo_region_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-// TODO: no refs
+// TODO: no refs used in any s3d - Tacc
 func TestWLD_activeGeoRegionRead(t *testing.T) {
 	e, err := New("test", nil)
 	if err != nil {
@@ -14,7 +14,7 @@ func TestWLD_activeGeoRegionRead(t *testing.T) {
 	fragmentTests(t,
 		true, //single run stop
 		[]string{
-			"gequip.s3d",
+			"yxtta_obj.s3d",
 		},
 		35,                    //fragCode
 		-1,                    //fragIndex
