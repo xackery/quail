6bd781912d32df4e465f744f703e004916773dd2
Xackery <xackery@gmail.com>
2023-04-30 12:36:16 -0700

    Add TODO for track

--- model/mesh/wld/z_19_track.go
+++ model/mesh/wld/z_19_track.go
@@ -23,7 +23,9 @@ func (e *WLD) trackRead(r io.ReadSeeker, fragmentOffset int) error {
 	def.nameRef = dec.Int32()
 	def.trackRef = dec.Int32()
 	def.flags = dec.Uint32()
+	// TODO: find an example with a flag sleep enabled
 	if def.flags&0x01 == 0x01 {
+		log.Debugf("Found a sleep enabled track!")
 		def.sleep = dec.Uint32()
 	}
 
