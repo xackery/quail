059a0ea137ce223baa7a3038b79ac9c30ee4c38e
Xackery Xtal <xackery@gmail.com>
2023-09-15 10:33:49 -0700

    Add lowercase warning

--- helper/crc.go
+++ helper/crc.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io"
 	"strings"
+
+	"github.com/xackery/quail/log"
 )
 
 var (
@@ -46,6 +48,7 @@ var (
 		0xBDBDF21C, 0xCABAC28A, 0x53B39330, 0x24B4A3A6, 0xBAD03605, 0xCDD70693, 0x54DE5729, 0x23D967BF,
 		0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94, 0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D,
 	}
+	isWarnedLowercase = false
 )
 
 func init() {
@@ -65,6 +68,11 @@ func init() {
 
 // FilenameCRC32 returns the CRC of a file's name
 func FilenameCRC32(name string) uint32 {
+	if !isWarnedLowercase && strings.ToLower(name) != name {
+		isWarnedLowercase = true
+		log.Infof("FilenameCRC32: %s is not lowercase, forcing it but EQ may fail to load this", name)
+	}
+
 	name = strings.ToLower(name)
 	if len(name) == 0 {
 		return 0
