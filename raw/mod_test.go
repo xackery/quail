4953c5a7d08141ccd02f7dd84abb0aec42abd3c1
xackery <xackery@gmail.com>
2023-11-01 21:27:23 -0700

    Add mod write

--- raw/mod_test.go
+++ raw/mod_test.go
@@ -50,3 +50,75 @@ func TestModRead(t *testing.T) {
 		})
 	}
 }
+
+func TestModWrite(t *testing.T) {
+	eqPath := os.Getenv("EQ_PATH")
+	if eqPath == "" {
+		t.Skip("EQ_PATH not set")
+	}
+	tests := []struct {
+		eqg     string
+		file    string
+		wantErr bool
+	}{
+
+		// .mod|0|obp_fob_tree.mod|oldfieldofbone.eqg oldfieldofbone.eqg pfs import: readMod obp_fob_tree.mod: invalid header EQLO, wanted EQGM
+		// .mod|0|obp_fob_tree.mod|oldfieldofboneb.eqg oldfieldofboneb.eqg pfs import: readMod obp_fob_tree.mod: invalid header EQLO, wanted EQGM
+		// .mod|1|arch.mod|dranik.eqg
+		// .mod|1|aro.mod|aro.eqg
+		// .mod|1|col_b04.mod|b04.eqg b04.eqg pfs import: readMod col_b04.mod: material shader not found
+		// .mod|2|boulder_lg.mod|broodlands.eqg
+		// .mod|2|et_door01.mod|stillmoona.eqg
+		// .mod|3|.mod|paperbaghat.eqg
+		// .mod|3|it11409.mod|undequip.eqg
+		{eqg: "undequip.eqg", file: "it11409.mod"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.eqg, func(t *testing.T) {
+			archive, err := pfs.NewFile(eqPath + "/" + tt.eqg)
+			if err != nil {
+				t.Fatalf("pfs.NewFile() error = %v", err)
+			}
+
+			data, err := archive.File(tt.file)
+			if err != nil {
+				t.Fatalf("archive.Open() error = %v", err)
+			}
+
+			mod := &Mod{}
+			err = mod.Read(bytes.NewReader(data))
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("Decode() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			buf := bytes.NewBuffer(nil)
+			err = mod.Write(buf)
+			if err != nil {
+				t.Fatalf("Encode() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			mod2 := &Mod{}
+			err = mod2.Read(bytes.NewReader(buf.Bytes()))
+			if err != nil {
+				t.Fatalf("Decode() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if len(mod.Materials) != len(mod2.Materials) {
+				t.Errorf("Materials mismatch, got %d, expected %d", len(mod.Materials), len(mod2.Materials))
+			}
+
+			if len(mod.Vertices) != len(mod2.Vertices) {
+				t.Errorf("Vertices mismatch, got %d, expected %d", len(mod.Vertices), len(mod2.Vertices))
+			}
+
+			if len(mod.Triangles) != len(mod2.Triangles) {
+				t.Errorf("Triangles mismatch, got %d, expected %d", len(mod.Triangles), len(mod2.Triangles))
+			}
+
+			if len(mod.Bones) != len(mod2.Bones) {
+				t.Errorf("Bones mismatch, got %d, expected %d", len(mod.Bones), len(mod2.Bones))
+			}
+
+		})
+	}
+}
--- raw/mod_write.go
+++ raw/mod_write.go
@@ -1,70 +1,130 @@
 package raw
 
 import (
+	"encoding/binary"
+	"fmt"
 	"io"
+	"strconv"
+
+	"github.com/xackery/encdec"
 )
 
 // Encode writes a mod file
 func (mod *Mod) Write(w io.Writer) error {
-	/*var err error
-	modelNames := []string{}
-
-	if len(model.Bones) > 0 {
-		modelNames = append(modelNames, model.Header.Name)
-	}
-
-	names, nameData, err := model.NameBuild(modelNames)
-	if err != nil {
-		return fmt.Errorf("nameBuild: %w", err)
-	}
-
-	materialData, err := model.MaterialBuild(names)
-	if err != nil {
-		return fmt.Errorf("materialBuild: %w", err)
-	}
-
-	verticesData, err := model.VertexBuild(version, names)
-	if err != nil {
-		return fmt.Errorf("vertexBuild: %w", err)
-	}
-
-	triangleData, err := model.TriangleBuild(version, names)
-	if err != nil {
-		return fmt.Errorf("triangleBuild: %w", err)
-	}
-
-	boneData, err := model.BoneBuild(version, "mod", names)
-	if err != nil {
-		return fmt.Errorf("boneBuild: %w", err)
-	}
-
-	tag.New()
+	var err error
 	enc := encdec.NewEncoder(w, binary.LittleEndian)
 	enc.String("EQGM")
-	enc.Uint32(version)
-	enc.Uint32(uint32(len(nameData)))
-	enc.Uint32(uint32(len(model.Materials)))
-	enc.Uint32(uint32(len(model.Vertices)))
-	enc.Uint32(uint32(len(model.Triangles)))
-	enc.Uint32(uint32(len(model.Bones)))
-	tag.Add(0, enc.Pos()-1, "red", "header")
+	enc.Uint32(mod.Version)
+
+	NameClear()
+
+	for _, material := range mod.Materials {
+		NameAdd(material.Name)
+		NameAdd(material.ShaderName)
+		for _, prop := range material.Properties {
+			_, err = strconv.Atoi(prop.Value)
+			if err != nil {
+				NameAdd(prop.Value)
+			}
+		}
+	}
+
+	for _, bone := range mod.Bones {
+		NameAdd(bone.Name)
+	}
+
+	nameData := NameData()
+	enc.Uint32(uint32(len(nameData))) // nameLength
+	enc.Uint32(uint32(len(mod.Materials)))
+	enc.Uint32(uint32(len(mod.Vertices)))
+	enc.Uint32(uint32(len(mod.Triangles)))
+	enc.Uint32(uint32(len(mod.Bones)))
 	enc.Bytes(nameData)
-	tag.Add(tag.LastPos(), enc.Pos(), "green", "names")
-	enc.Bytes(materialData)
-	tag.Add(tag.LastPos(), enc.Pos(), "blue", "materials")
-	enc.Bytes(verticesData)
-	tag.Add(tag.LastPos(), enc.Pos(), "yellow", "vertices")
-	enc.Bytes(triangleData)
-	tag.Add(tag.LastPos(), enc.Pos(), "purple", "triangles")
-	enc.Bytes(boneData)
-	tag.Add(tag.LastPos(), enc.Pos(), "orange", "bones")
+
+	for _, material := range mod.Materials {
+		enc.Int32(material.ID)
+		enc.Uint32(uint32(NameIndex(material.Name)))
+		enc.Uint32(uint32(NameIndex(material.ShaderName)))
+		enc.Uint32(uint32(len(material.Properties)))
+		for _, prop := range material.Properties {
+			enc.Uint32(uint32(NameIndex(prop.Name)))
+			enc.Uint32(uint32(prop.Category))
+			switch prop.Category {
+			case 0:
+				fval, err := strconv.ParseFloat(prop.Value, 32)
+				if err != nil {
+					return fmt.Errorf("parse float: %w", err)
+				}
+				enc.Float32(float32(fval))
+			case 2:
+				enc.Int32(NameIndex(prop.Value))
+			default:
+				val, err := strconv.Atoi(prop.Value)
+				if err != nil {
+					return fmt.Errorf("parse int: %w", err)
+				}
+				enc.Int32(int32(val))
+			}
+		}
+	}
+
+	for _, vert := range mod.Vertices {
+		enc.Float32(vert.Position.X)
+		enc.Float32(vert.Position.Y)
+		enc.Float32(vert.Position.Z)
+		enc.Float32(vert.Normal.X)
+		enc.Float32(vert.Normal.Y)
+		enc.Float32(vert.Normal.Z)
+		if mod.Version > 2 {
+			enc.Uint8(vert.Tint.R)
+			enc.Uint8(vert.Tint.G)
+			enc.Uint8(vert.Tint.B)
+			enc.Uint8(vert.Tint.A)
+		}
+		enc.Float32(vert.Uv.X)
+		enc.Float32(vert.Uv.Y)
+		if mod.Version > 2 {
+			enc.Float32(vert.Uv2.X)
+			enc.Float32(vert.Uv2.Y)
+		}
+	}
+
+	for _, tri := range mod.Triangles {
+		enc.Uint32(tri.Index.X)
+		enc.Uint32(tri.Index.Y)
+		enc.Uint32(tri.Index.Z)
+		matID := int32(0)
+		for _, mat := range mod.Materials {
+			if mat.Name == tri.MaterialName {
+				matID = mat.ID
+				break
+			}
+		}
+		enc.Int32(matID)
+		enc.Uint32(tri.Flag)
+	}
+
+	for _, bone := range mod.Bones {
+		enc.Int32(NameIndex(bone.Name))
+		enc.Int32(bone.Next)
+		enc.Uint32(bone.ChildrenCount)
+		enc.Int32(bone.ChildIndex)
+		enc.Float32(bone.Pivot.X)
+		enc.Float32(bone.Pivot.Y)
+		enc.Float32(bone.Pivot.Z)
+		enc.Float32(bone.Rotation.X)
+		enc.Float32(bone.Rotation.Y)
+		enc.Float32(bone.Rotation.Z)
+		enc.Float32(bone.Scale.X)
+		enc.Float32(bone.Scale.Y)
+		enc.Float32(bone.Scale.Z)
+		enc.Float32(bone.Scale2)
+	}
 
 	err = enc.Error()
 	if err != nil {
 		return fmt.Errorf("encode: %w", err)
 	}
 
-	log.Debugf("%s encoded %d verts, %d triangles, %d bones, %d materials", model.Header.Name, len(model.Vertices), len(model.Triangles), len(model.Bones), len(model.Materials))
-	*/
 	return nil
 }
