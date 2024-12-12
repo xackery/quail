f16173c8d937980f174b064068c8c1f503c0db5c
Xackery <xackery@gmail.com>
2024-12-12 11:02:30 -0800

    Add json convert support prototype

---
+++ quail/json_read.go
@@ -0,0 +1,59 @@
+package quail
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/xackery/quail/wce"
+)
+
+// JsonRead loads a .json path
+func (q *Quail) JsonRead(path string) error {
+
+	fi, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if fi.IsDir() {
+		return fmt.Errorf("path %s is a directory, but should be a file", path+"/_root.wce")
+	}
+
+	baseName := filepath.Base(path)
+	baseName = strings.TrimSuffix(baseName, ".json")
+	q.Wld, err = wce.ReadJSON(baseName+".wld", path)
+	if err != nil {
+		return fmt.Errorf("json read: %w", err)
+	}
+
+	q.Textures = make(map[string][]byte)
+
+	path = filepath.Dir(path)
+	dirs, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+	for _, dir := range dirs {
+		if dir.IsDir() {
+			continue
+		}
+		ext := filepath.Ext(dir.Name())
+		if ext == ".wce" {
+			continue
+		}
+		if ext == ".mod" {
+			continue
+		}
+		if ext == ".json" {
+			continue
+		}
+		textureData, err := os.ReadFile(path + "/" + dir.Name())
+		if err != nil {
+			return err
+		}
+		q.Textures[dir.Name()] = textureData
+	}
+
+	return nil
+}
---
+++ quail/json_write.go
@@ -0,0 +1,50 @@
+package quail
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// DirWrite exports the quail target to a directory
+func (q *Quail) JsonWrite(path string) error {
+
+	err := os.MkdirAll(filepath.Dir(path), 0755)
+	if err != nil {
+		return err
+	}
+
+	if q.Wld != nil {
+		err = q.Wld.WriteJSON(path)
+		if err != nil {
+			return err
+		}
+	}
+	if q.WldObject != nil {
+		err = q.WldObject.WriteJSON(strings.TrimSuffix(path, ".json") + "_objects.json")
+		if err != nil {
+			return fmt.Errorf("write object: %w", err)
+		}
+	}
+	if q.WldLights != nil {
+		err = q.WldLights.WriteJSON(strings.TrimSuffix(path, ".json") + "_lights.json")
+		if err != nil {
+			return fmt.Errorf("write lights: %w", err)
+		}
+	}
+
+	for name, data := range q.Textures {
+
+		/* data, err := fixWonkyDDS(name, texture)
+		if err != nil {
+			return err
+		} */
+		err = os.WriteFile(path+"/"+name, data, 0644)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
