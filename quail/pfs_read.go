a2d3b60f76950e06ef3fddc8c6bc9ff582fd674b
Xackery <xackery@gmail.com>
2025-01-18 03:50:37 -0800

    Fix asset convert

--- quail/pfs_read.go
+++ quail/pfs_read.go
@@ -43,6 +43,19 @@ func (q *Quail) PfsRead(path string) error {
 
 	for _, file := range pfs.Files() {
 		ext := strings.ToLower(filepath.Ext(file.Name()))
+		reader, err := raw.Read(ext, bytes.NewReader(file.Data()))
+		if err != nil {
+			return fmt.Errorf("read %s: %w", file.Name(), err)
+		}
+		reader.SetFileName(file.Name())
+		err = q.RawRead(reader)
+		if err != nil {
+			return fmt.Errorf("rawRead %s: %w", file.Name(), err)
+		}
+	}
+
+	for _, file := range pfs.Files() {
+		ext := strings.ToLower(filepath.Ext(file.Name()))
 		if ext == ".wld" {
 			rawWld := &raw.Wld{}
 			err = rawWld.Read(bytes.NewReader(file.Data()))
