a5241be5cb62b79b6cf630acd6248ff32ab579f7
Xackery <xackery@gmail.com>
2025-03-27 16:31:27 -0700

    Fix lod byte precision

--- raw/lod_write.go
+++ raw/lod_write.go
@@ -3,18 +3,28 @@ package raw
 import (
 	"fmt"
 	"io"
+	"strings"
 )
 
 func (lod *Lod) Write(w io.Writer) error {
 	// Write the header
-	_, err := w.Write([]byte("EQLOD\n"))
+	_, err := w.Write([]byte("EQLOD\r\n"))
 	if err != nil {
 		return fmt.Errorf("failed to write header: %w", err)
 	}
 
 	// Write each entry
 	for _, entry := range lod.Entries {
-		line := fmt.Sprintf("%s,%s,%0.3f\n", entry.Category, entry.ObjectName, entry.Distance)
+		distance := fmt.Sprintf("%06.3f", entry.Distance)
+		if strings.HasSuffix(distance, ".000") {
+			distance = strings.TrimSuffix(distance, ".000")
+		}
+
+		line := fmt.Sprintf("%s,%s", entry.Category, entry.ObjectName)
+		if distance != "00" {
+			line += "," + distance
+		}
+		line += "\r\n"
 		_, err := w.Write([]byte(line))
 		if err != nil {
 			return fmt.Errorf("failed to write entry: %w", err)
