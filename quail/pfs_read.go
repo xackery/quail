00804c917d72bda8db2b59fd7ef05aebe574d567
Xackery <xackery@gmail.com>
2025-03-29 11:31:24 -0700

    Fix error message terms

--- quail/pfs_read.go
+++ quail/pfs_read.go
@@ -46,12 +46,12 @@ func (q *Quail) PfsRead(path string) error {
 		}
 		reader, err := raw.Read(ext, bytes.NewReader(file.Data()))
 		if err != nil {
-			return fmt.Errorf("read %s: %w", file.Name(), err)
+			return fmt.Errorf("raw.Read %s: %w", file.Name(), err)
 		}
 		reader.SetFileName(file.Name())
 		err = q.RawRead(reader)
 		if err != nil {
-			return fmt.Errorf("rawRead %s: %w", file.Name(), err)
+			return fmt.Errorf("q rawRead %s: %w", file.Name(), err)
 		}
 	}
 
